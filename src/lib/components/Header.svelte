157c6f458715a4c074da7d3b99f822247261df10
jssdestroyerman <jesuisdestroyerman@proton.me>
2023-03-06 18:48:46 +0100

    base

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,28 @@
+<header class="flex justify-between px-20 pt-16">
+	<h2 class="text-xl font-medium upp">cursorto</h2>
+
+	<nav>
+		<ul class="flex w-[200px] justify-between items-center">
+			<li>showreel</li>
+			<li>menu</li>
+			<li>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-6 h-6"
+				>
+					<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
+				</svg>
+			</li>
+		</ul>
+	</nav>
+</header>
+
+<style lang="postcss">
+	li {
+		@apply hover:opacity-70 cursor-pointer;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+	import Header from '../lib/components/Header.svelte';
+</script>
+
+<div class="h-[100vh] flex flex-col">
+	<Header />
+	<main class="flex justify-center px-[250px] flex-col grow">
+		<p>We make it happen</p>
+		<h1><span>Websites</span> <span>Apps</span> <span>Branding</span></h1>
+	</main>
+</div>
+
+<style>
+	h1 {
+		@apply text-[110px] font-bold;
+		-webkit-text-fill-color: transparent;
+		-webkit-text-stroke: 1px;
+	}
+	span {
+		@apply transition-all duration-300;
+	}
+	span:hover {
+		@apply cursor-pointer;
+		-webkit-text-fill-color: black;
+	}
+</style>
