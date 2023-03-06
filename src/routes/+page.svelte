e1917751b00725c4a5fe6b641ca5eee278922da0
jssdestroyerman <jesuisdestroyerman@proton.me>
2023-03-06 19:07:36 +0100

    cursor moving

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,31 @@
 <script>
 	import Header from '../lib/components/Header.svelte';
+	import { spring } from 'svelte/motion';
+
+	let coords = spring(
+		{ x: 50, y: 50 },
+		{
+			stiffness: 0.05,
+			damping: 0.5
+		}
+	);
 </script>
 
-<div class="h-[100vh] flex flex-col">
+<div
+	class="h-[100vh] flex flex-col"
+	on:mousemove={(e) => coords.set({ x: e.clientX, y: e.clientY })}
+>
 	<Header />
 	<main class="flex justify-center px-[250px] flex-col grow">
 		<p>We make it happen</p>
 		<h1><span>Websites</span> <span>Apps</span> <span>Branding</span></h1>
 	</main>
+
+	<div
+		class={`h-5 w-5 rounded-full bg-black absolute`}
+		style={`transform: translate(${$coords.x}px, ${$coords.y}px);
+		`}
+	/>
 </div>
 
 <style>
