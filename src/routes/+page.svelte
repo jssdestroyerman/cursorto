aa1e93bd8050084ddabc24b460dbacd612f4885f
jssdestroyerman <jesuisdestroyerman@proton.me>
2023-03-07 12:21:25 +0100

    scaling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,14 +16,26 @@
 
 <div class="h-[100vh] flex flex-col">
 	<Header />
-	<main class="flex justify-center px-[250px] flex-col grow">
+	<main
+		class="flex justify-center px-[250px] flex-col grow"
+		on:mouseenter={() => {
+			gsap.to('.customCursor', {
+				scale: 5
+			});
+		}}
+		on:mouseleave={() => {
+			gsap.to('.customCursor', {
+				scale: 1
+			});
+		}}
+	>
 		<p>We make it happen</p>
 		<h1>
 			<span>Websites</span> <span>Apps</span> <span>Branding</span>
 		</h1>
 	</main>
 
-	<div class="h-8 w-8 rounded-full bg-black absolute customCursor" />
+	<div class="h-8 w-8 rounded-full bg-black absolute customCursor -z-20" />
 </div>
 
 <style>
